849bd91888886d0046fa243d9d59a6c549b177c2
苏桓 <152363249+Dann-Zeng@users.noreply.github.com>
2024-05-26 15:19:18 +0800

    Update SwichCaseFallThrough.java

--- ShangSiliconValley/SwichCaseFallThrough.java
+++ ShangSiliconValley/SwichCaseFallThrough.java
@@ -1,47 +1,76 @@
+/*
+从键盘分别输入年、月、日，判断这一天是当年的第几天
+ 
+注：判断一年是否是闰年的标准：
+   1）可以被4整除，但不可被100整除
+   或
+   2）可以被400整除
+   
+例如：1900，2200等能被4整除，但同时能被100整除，但不能被400整除，不是闰年
+*/
 import java.util.Scanner;
 
-class SwitchCaseFallThrough {
- public static void main(String[] args) {
-  
-  Scanner scan = new Scanner(System.in);
+public class SwitchCaseTest04 {
 
-  System.out.println("请输入2024年的month:");
-  int month = scan.nextInt();
+    public static void main(String[] args) {
 
-  System.out.println("请输入2024年的day:");
-  int day = scan.nextInt();
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("请输入year:");
+        int year = scanner.nextInt();
 
-  int sumDays = 0;//记录总天数
+        System.out.print("请输入month:");
+        int month = scanner.nextInt();
 
-  switch(month){
-   case 12:
-    sumDays += 30;//这个30是代表11月份的满月天数
-   case 11:
-    sumDays += 31;//这个31是代表10月份的满月天数
-   case 10:
-    sumDays += 30;//这个30是代表9月份的满月天数
-   case 9:
-    sumDays += 31;//这个31是代表8月份的满月天数
-   case 8:
-    sumDays += 31;//这个31是代表7月份的满月天数
-   case 7:
-    sumDays += 30;//这个30是代表6月份的满月天数
-   case 6:
-    sumDays += 31;//这个31是代表5月份的满月天数
-   case 5:
-    sumDays += 30;//这个30是代表4月份的满月天数
-   case 4:
-    sumDays += 31;//这个31是代表3月份的满月天数
-   case 3:
-    sumDays += 29;//这个28是代表2月份的满月天数
-   case 2:
-    sumDays += 31;//这个31是代表1月份的满月天数
-   case 1:
-    sumDays += day;//这个day是代表当月的第几天
-  }
-  
-  System.out.println(month + "月" + day + "日是2024年的第" + sumDays + "天");
-        //关闭资源
-  scan.close();
- }
+        System.out.print("请输入day:");
+        int day = scanner.nextInt();
+
+        //判断这一天是当年的第几天==>从1月1日开始，累加到xx月xx日这一天
+        //(1)[1,month-1]个月满月天数
+        //(2)单独考虑2月份是否是29天（依据是看year是否是闰年）
+        //(3)第month个月的day天
+
+        //声明一个变量days，用来存储总天数
+        int sumDays = 0;
+
+        //累加[1,month-1]个月满月天数
+        switch (month) {
+            case 12:
+                //累加的1-11月
+                sumDays += 30;//这个30是代表11月份的满月天数
+                //这里没有break，继续往下走
+            case 11:
+                //累加的1-10月
+                sumDays += 31;//这个31是代表10月的满月天数
+                //这里没有break，继续往下走
+            case 10:
+                sumDays += 30;//9月
+            case 9:
+                sumDays += 31;//8月
+            case 8:
+                sumDays += 31;//7月
+            case 7:
+                sumDays += 30;//6月
+            case 6:
+                sumDays += 31;//5月
+            case 5:
+                sumDays += 30;//4月
+            case 4:
+                sumDays += 31;//3月
+            case 3:
+                sumDays += 28;//2月
+                //在这里考虑是否可能是29天
+                if (year % 4 == 0 && year % 100 != 0 || year % 400 == 0) {
+                    sumDays++;//多加1天
+                }
+            case 2:
+                sumDays += 31;//1月
+            case 1:
+                sumDays += day;//第month月的day天
+        }
+
+        //输出结果
+        System.out.println(year + "年" + month + "月" + day + "日是这一年的第" + sumDays + "天");
+
+        scanner.close();
+    }
 }
